4e2fbae5c1311bd789b6b97abb76a187c3bb7e3a
Sean Breckenridge <seanbrecke@gmail.com>
2020-03-15 15:40:05 -0700

    add new choices for dvd text

--- choices.js
+++ choices.js
@@ -1,1 +1,26 @@
-let choices = ["DVD", "XQC", "HDTV"]
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+
+let choices = shuffle(["DVD", "XQC", "HDTV", "PVC", "HTC", "STD", "LSD", "ABC",
+              "XYZ", "123", "PSP", "KFC", "SSD", "HDD", "PTSD", "THC",
+              "PS3", "MP3", "AMD", "BRB", "MTV", "RPG", "LCD", "TNT",
+              "HIV", "USB", "USD", "SUV", "TTV", "DLC", "PVP", "NYC", "DDR3",
+              "CRT", "PNG", "DLC", "RNG", "ADHD", "4KTV", "NPC", "MLG", "PHD",
+              "LGBT"])
