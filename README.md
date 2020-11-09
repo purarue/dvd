883c9caaea909e36575a718e75b847f2351d4d14
Sean Breckenridge <seanbrecke@gmail.com>
2020-11-09 01:42:48 -0800

    autoformat

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# XQC-DVD  [![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com) 
+# XQC-DVD [![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
 
 <img src="https://raw.githubusercontent.com/seanbreckenridge/xqc-dvd/master/.github/demo.gif" width=600>
 
@@ -10,9 +10,9 @@ A browser dvd logo animation/game.
 
 ## Install
 
-* If you want, edit the array in `choices.js` file, which defines the text on the screen after each bounce.
-* `make`
-* open `index.html` in your browser
+- If you want, edit the array in `choices.js` file, which defines the text on the screen after each bounce.
+- `make`
+- open `index.html` in your browser
 
 Generates a static site with this git directory as the root.
 
--- choices.js
+++ choices.js
@@ -1,9 +1,10 @@
 function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
-
     // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
@@ -17,10 +18,48 @@ function shuffle(array) {
   return array;
 }
 
-
-let choices = shuffle(["DVD", "XQC", "HDTV", "PVC", "HTC", "STD", "LSD", "ABC",
-              "XYZ", "123", "PSP", "KFC", "SSD", "HDD", "PTSD", "THC",
-              "PS3", "MP3", "AMD", "BRB", "MTV", "RPG", "LCD", "TNT",
-              "HIV", "USB", "USD", "SUV", "TTV", "DLC", "PVP", "NYC", "DDR3",
-              "CRT", "PNG", "DLC", "RNG", "ADHD", "4KTV", "NPC", "MLG", "PHD",
-              "LGBT"])
+let choices = shuffle([
+  "DVD",
+  "XQC",
+  "HDTV",
+  "PVC",
+  "HTC",
+  "STD",
+  "LSD",
+  "ABC",
+  "XYZ",
+  "123",
+  "PSP",
+  "KFC",
+  "SSD",
+  "HDD",
+  "PTSD",
+  "THC",
+  "PS3",
+  "MP3",
+  "AMD",
+  "BRB",
+  "MTV",
+  "RPG",
+  "LCD",
+  "TNT",
+  "HIV",
+  "USB",
+  "USD",
+  "SUV",
+  "TTV",
+  "DLC",
+  "PVP",
+  "NYC",
+  "DDR3",
+  "CRT",
+  "PNG",
+  "DLC",
+  "RNG",
+  "ADHD",
+  "4KTV",
+  "NPC",
+  "MLG",
+  "PHD",
+  "LGBT",
+]);
